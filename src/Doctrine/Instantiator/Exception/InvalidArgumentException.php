b1f6e09e7b4d7003a4723f252d978399d27dfbcf
Michael Moravec <me@majkl.me>
2019-02-20 05:47:50 +0100

    Enable strict types

--- src/Doctrine/Instantiator/Exception/InvalidArgumentException.php
+++ src/Doctrine/Instantiator/Exception/InvalidArgumentException.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Doctrine\Instantiator\Exception;
 
 use InvalidArgumentException as BaseInvalidArgumentException;
--- tests/DoctrineTest/InstantiatorTest/Exception/UnexpectedValueExceptionTest.php
+++ tests/DoctrineTest/InstantiatorTest/Exception/UnexpectedValueExceptionTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace DoctrineTest\InstantiatorTest\Exception;
 
 use Doctrine\Instantiator\Exception\UnexpectedValueException;
